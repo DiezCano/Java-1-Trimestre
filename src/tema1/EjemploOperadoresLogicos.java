158dc9bd7d06910970434d3aea82bc1adc03eb6b
DiezCano <jdiecan2926@gmail.com>
2025-09-29 17:25:48 +0200

    Ejemplos Operadores

---
+++ src/tema1/EjemploOperadoresLogicos.java
@@ -0,0 +1,32 @@
+package tema1;
+
+public class EjemploOperadoresLogicos {
+    public static void main(String[] args) {
+
+        double precio1 = 9.99, precio2 = 8.99 , descuento = 1.0;
+        boolean resultado;
+
+        //1. precio1 >= 9 && descuento >= 1
+        resultado = (precio1 >= 9 && descuento >= 1);
+        System.out.println(resultado);
+
+        //2. precio1 >= 10 0 precio2 <= 10
+        resultado = (precio1 >= 10 || precio2 <= 10);
+        System.out.println(resultado);
+
+        //3. precio1 >= 9 Y precio2 >= 5 Y descuento > 0.5)
+        resultado = ! (precio1 >= 9 && precio2 >= 5 && descuento >= 0.5);
+        System.out.println(resultado);
+
+        //4. (!(precio1 >= precio2)) O (precio2 < 9)
+        resultado = (! (precio1 >= precio2)) || (precio2 < 9);
+        System.out.println(resultado);
+
+        //5. (precio1 <= precio2) || (descuento1 > 1) ) && (precio2 < 10)
+        resultado = (precio1 <= precio2) || (descuento > 1) && (precio2 < 10);
+        System.out.println(resultado);
+
+
+
+    }
+}
