78a3c4f6ab6fa02ba4af82c344e593de1ab37f0a
DiezCano <jdiecan2926@gmail.com>
2025-10-06 17:41:09 +0200

    Ejemplos Operadores

---
+++ src/tema1/EjercicioClaseJesus8.java
@@ -0,0 +1,18 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class EjercicioClaseJesus8 {
+    public static void main(String[] args) {
+
+        int respuestasBien = 6;
+        int respuestasMal = 7;
+        int respuestasSinRes = 4;
+
+        int nota = (5*respuestasBien) + (respuestasMal - 1);
+        System.out.println("Nota: " + nota);
+
+
+
+    }
+}
