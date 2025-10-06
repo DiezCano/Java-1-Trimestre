675c49a70a14d8ca7c3ccd7b8ba7cda2f30f4fd7
DiezCano <jdiecan2926@gmail.com>
2025-10-06 17:33:35 +0200

    Ejemplos Operadores

--- src/tema1/EjerciciosClaseJesus6.java
+++ src/tema1/EjerciciosClaseJesus6.java
@@ -3,7 +3,16 @@ package tema1;
 public class EjerciciosClaseJesus6 {
     public static void main(String[] args) {
 
+        double x1,x2,x3,x4;
+        double distancia;
 
+        x1=45.66;
+        x2=69.46;
+        x3=67.16;
+        x4=12.23;
+
+        distancia =Math.sqrt (Math.pow ((x2-x1),2) + Math.pow ((x2-x2),2) + Math.pow ((x4-x3),2));
+        System.out.println("Distancia: " + distancia);
 
 
     }
---
+++ src/tema1/EjerciciosClaseJesus7.java
@@ -0,0 +1,19 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class EjerciciosClaseJesus7 {
+    public static void main(String[] args) {
+        int numero = 36;
+        int resultado; //63;
+        int digito1 = numero / 10;
+        int digito2 = numero % 10;
+
+        resultado = (6*10) + 3;
+        System.out.println("Resultado: " + resultado);
+
+
+
+
+    }
+}
