14cc2c54a8457ca91a2b11da60a47572fd74ecd9
DiezCano <jdiecan2926@gmail.com>
2025-11-06 16:04:23 +0100

    tema2

---
+++ src/Repaso/EjercicioRepaso.java
@@ -0,0 +1,34 @@
+package Repaso;
+
+import java.util.Scanner;
+
+public class EjercicioRepaso {
+    static void main() {
+         /*
+        Programa que lea por teclado una serie de números enteros hasta que
+        introduzca el -1 y obtenga su media. Deberá mostrarla por pantalla.
+         */
+
+        double media=0; // acumulado / numero de elementos
+        int numero=0, acumulador=0, contador=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (numero != -1) {
+            System.out.println("Introduce número: ");
+            numero = Integer.parseInt(sc.nextLine());
+
+            if (numero == -1) {
+                break;
+            }
+
+            acumulador += numero;
+            contador++;
+
+
+        }
+
+        media = (double) acumulador / contador;
+        System.out.println("La media es: " + media);
+    }
+}
