f459aa818b0400fb7090db4e8200e74e5b3cb68d
DiezCano <jdiecan2926@gmail.com>
2025-11-28 18:53:04 +0100

    ExamenEntornos.java

---
+++ src/ExamenEntornos.java
@@ -0,0 +1,9 @@
+public class ExamenEntornos {
+    public static void main(String[] args) {
+
+        double sueldo = 2000.00;
+        double gastos = 1200.00;
+        double ahorro = sueldo - gastos;
+        System.out.println("El ahorro del empleado es: " + ahorro + "€");
+    }
+}
